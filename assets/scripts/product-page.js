f86c529767e7f683d449b4c0be947795d8f4f310
Albi <albi.c@msn.com>
2023-01-15 16:41:13 +0100

    product page

--- assets/scripts/product-page.js
+++ assets/scripts/product-page.js
@@ -8,8 +8,8 @@ const swiper = new Swiper('.thumb-product-slider', {
 const swiper2 = new Swiper('.main-product-slider', {
   spaceBetween: 10,
   navigation: {
-    nextEl: '.swiper-button-next',
-    prevEl: '.swiper-button-prev',
+    nextEl: '.main-product-slider .swiper-button-next',
+    prevEl: '.main-product-slider .swiper-button-prev',
   },
   thumbs: {
     swiper: swiper,
@@ -24,3 +24,40 @@ const secondSlider = document.querySelector(
   '.thumb-product-slider .swiper-wrapper'
 )
 secondSlider.innerHTML = firstSlider.innerHTML
+
+function initTab(elem) {
+  document.addEventListener('click', function (e) {
+    if (!e.target.matches(elem + ' .tab')) return
+    else {
+      if (!e.target.classList.contains('active')) {
+        findActiveElementAndRemoveIt(elem + ' .tab')
+        findActiveElementAndRemoveIt(elem + ' .tab-content')
+        e.target.classList.add('active')
+        setTimeout(function () {
+          var panel = document.querySelectorAll(
+            elem + ' .tab-content.' + e.target.dataset.name
+          )
+          Array.prototype.forEach.call(panel, function (el) {
+            el.classList.add('active')
+          })
+        }, 200)
+      }
+    }
+  })
+}
+
+function findActiveElementAndRemoveIt(elem) {
+  var elementList = document.querySelectorAll(elem)
+  Array.prototype.forEach.call(elementList, function (e) {
+    e.classList.remove('active')
+  })
+}
+
+initTab('.tab-holder')
+
+// get the width of the first tab and set it to the table tr td:first-child
+// const firstTab = document.querySelector('.tab-holder .tab')
+// const firstTabWidth = firstTab.offsetWidth
+
+// const tableFirstTd = document.querySelector('.tab-holder table td:first-child')
+// tableFirstTd.style.width = firstTabWidth + 'px'
