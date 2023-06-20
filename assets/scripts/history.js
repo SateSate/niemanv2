dbcda24c8a6f3241b6d8642d3c88c50264112a5a
Albi Cenga <albi.cenga@jakala.com>
2023-06-21 01:46:18 +0200

    pagination

--- assets/scripts/history.js
+++ assets/scripts/history.js
@@ -2,13 +2,18 @@ document.addEventListener('DOMContentLoaded', function () {
   const timeline = document.querySelector('.timeline');
   const yearsContainer = document.querySelector('.pagination .years');
   const years = Array.from(document.querySelectorAll('.timeline .year'));
+
   const yearButtons = years.map(function (year, index) {
+    const yearButtonHolder = document.createElement('div');
     const yearButton = document.createElement('div');
     const yearButtonSpan = document.createElement('span');
+    yearButtonHolder.classList.add('year-button-holder');
     yearButton.classList.add('year-button');
+    yearButtonSpan.classList.add('year-button-span');
     yearButtonSpan.innerText = year.dataset.year;
     yearButton.appendChild(yearButtonSpan);
-    yearsContainer.appendChild(yearButton);
+    yearButtonHolder.appendChild(yearButton);
+    yearsContainer.appendChild(yearButtonHolder);
 
     yearButton.addEventListener('click', function () {
       setActiveYear(index);
@@ -18,25 +23,24 @@ document.addEventListener('DOMContentLoaded', function () {
   });
 
   let activeYearIndex = 0;
-  let activeButtonIndex = Math.floor(years.length / 2);
   setActiveYear(activeYearIndex);
 
-  const prevButton = document.querySelector('.pagination .nav.prev');
-  const nextButton = document.querySelector('.pagination .nav.next');
+  // const prevButton = document.querySelector('.pagination .nav.prev');
+  // const nextButton = document.querySelector('.pagination .nav.next');
 
-  prevButton.addEventListener('click', function () {
-    if (activeYearIndex > 0) {
-      activeYearIndex--;
-      setActiveYear(activeYearIndex);
-    }
-  });
+  // prevButton.addEventListener('click', function () {
+  //   if (activeYearIndex > 0) {
+  //     activeYearIndex--;
+  //     setActiveYear(activeYearIndex);
+  //   }
+  // });
 
-  nextButton.addEventListener('click', function () {
-    if (activeYearIndex < years.length - 1) {
-      activeYearIndex++;
-      setActiveYear(activeYearIndex);
-    }
-  });
+  // nextButton.addEventListener('click', function () {
+  //   if (activeYearIndex < years.length - 1) {
+  //     activeYearIndex++;
+  //     setActiveYear(activeYearIndex);
+  //   }
+  // });
 
   function setActiveYear(index) {
     years.forEach(function (year, yearIndex) {
@@ -48,12 +52,10 @@ document.addEventListener('DOMContentLoaded', function () {
     });
 
     yearButtons.forEach(function (button, buttonIndex) {
-      if (buttonIndex === activeButtonIndex) {
+      if (buttonIndex === index) {
         button.classList.add('active');
         button.classList.remove('future');
-        // Set the button's text to the active year
-        button.children[0].innerText = years[activeYearIndex].dataset.year;
-      } else if (buttonIndex > activeButtonIndex) {
+      } else if (buttonIndex > index) {
         button.classList.add('future');
         button.classList.remove('active');
       } else {
@@ -67,17 +69,45 @@ document.addEventListener('DOMContentLoaded', function () {
 
     timeline.style.transform = `translateY(-${activeYearTop - timelineTop}px)`;
 
-    const visibleYears = years.filter(function (year, yearIndex) {
-      return yearIndex >= index;
-    });
-    const visibleYearsHeight = visibleYears.reduce(function (
-      totalHeight,
-      year
-    ) {
-      return totalHeight + year.offsetHeight;
-    },
-    0);
-    const timelineContainer = document.querySelector('.timeline-container');
-    timelineContainer.style.height = visibleYearsHeight + 'px';
+    const activeYearData = years[activeYearIndex].dataset.year;
+    const activeYearElement = document.querySelector('.year.active');
+    activeYearElement.querySelector('.about-text h2').innerText =
+      activeYearData;
+  }
+});
+
+// Get the necessary elements
+const prevButton = document.querySelector('.nav.prev');
+const nextButton = document.querySelector('.nav.next');
+const yearsContainer = document.querySelector('.pagination .years');
+
+// Set the initial translation value
+let translateValue = 0;
+
+// Define the translation amount in pixels
+const translationAmount = 400; // Adjust this value based on your desired translation
+
+// Add click event listener to the next button
+nextButton.addEventListener('click', function () {
+  // Calculate the maximum translate value
+  const maxTranslateValue = -(
+    yearsContainer.scrollWidth - yearsContainer.parentElement.offsetWidth
+  );
+
+  // Check if the current translate value is less than the maximum value
+  if (translateValue > maxTranslateValue) {
+    // Update the translate value
+    translateValue -= translationAmount;
+    yearsContainer.style.transform = `translateX(${translateValue}px)`;
+  }
+});
+
+// Add click event listener to the prev button
+prevButton.addEventListener('click', function () {
+  // Check if the current translate value is greater than zero
+  if (translateValue < 0) {
+    // Update the translate value
+    translateValue += translationAmount;
+    yearsContainer.style.transform = `translateX(${translateValue}px)`;
   }
 });
